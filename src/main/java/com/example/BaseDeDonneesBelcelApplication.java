2ab874360e7fe3a4c3effa50041a4dd0eb63798a
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-03 14:11:43 +0100

    Create type en principal class

--- src/main/java/com/example/BaseDeDonneesBelcelApplication.java
+++ src/main/java/com/example/BaseDeDonneesBelcelApplication.java
@@ -1,13 +1,51 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.entities.Type;
+import com.example.services.MotService;
+import com.example.services.TypeService;
+
+import lombok.RequiredArgsConstructor;
+
 @SpringBootApplication
-public class BaseDeDonneesBelcelApplication {
+@RequiredArgsConstructor
+public class BaseDeDonneesBelcelApplication implements CommandLineRunner {
+
+	private final MotService motService;
+	private final TypeService typeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaseDeDonneesBelcelApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// departamentos
+		Type type1 = Type.builder()
+			.type("NOM")
+			.build();
+		
+		Type type2 = Type.builder()
+			.type("ADJ")
+			.build();
+		
+		Type type3 = Type.builder()
+			.type("ADV")
+			.build();
+		
+		Type type4 = Type.builder()
+			.type("AUTRES")
+			.build();
+
+	typeService.persisterUnType(type1);
+	typeService.persisterUnType(type2);
+	typeService.persisterUnType(type3);
+	typeService.persisterUnType(type4);
+
 	}
 
 }
