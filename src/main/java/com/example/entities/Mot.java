ca33302a554e6d7a038ae7812abc7652373ba4c6
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-03 13:17:08 +0100

    Add MotDao Co-authored-by: Celia <Celudirosa@users.noreply.github.com>

--- src/main/java/com/example/entities/Mot.java
+++ src/main/java/com/example/entities/Mot.java
@@ -2,13 +2,26 @@ package com.example.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "mots")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Mot implements Serializable {
+    
     private static final long serialVersionUID = 1L;
 
     @Id
